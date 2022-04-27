3e55c9381be190f5b822f26beddb94e3c3de54d2
Abel Masila <abel@x2ai.com>
2022-04-27 03:10:33 +0300

    UI work done

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,29 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Abel Masila on 27/04/2022.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
