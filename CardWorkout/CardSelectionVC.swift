345685c90f132eea5a2eb5ccf63041fa784664e5
Abel Masila <abel@x2ai.com>
2022-04-27 17:02:20 +0300

    all features added.

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -13,24 +13,37 @@ class CardSelectionVC: UIViewController {
 
 
     @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Card.allValues
+    var timer: Timer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startTimer()
         for button in buttons{
             button.layer.cornerRadius = 8
         }
-
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
     }
 
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
 
-
+    }
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
 
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
-    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    @IBAction func restartButtonTapped(_ sender : UIButton) {
+        timer.invalidate()
+        startTimer()
     }
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
-    }
+
 
 }
